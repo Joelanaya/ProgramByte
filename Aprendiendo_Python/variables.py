440f3567992ea739bd45b8042514ede3173ff7da
Joelanaya <86157986+Joelanaya@users.noreply.github.com>
2024-06-07 14:21:52 -0500

    Variables

---
+++ Aprendiendo_Python/variables.py
@@ -0,0 +1,25 @@
+#Podemos declarar variables de texto, numeros enteros, decimales booleanos, listas etc.
+nombre = "Program Byte"
+numero = "Este es el año 2024"
+num = 2024
+
+print(nombre)
+print(numero)
+print(num)
+
+nombreapp = "Aprendiendo Python "
+nombreApp = "Aprendiendo ..."
+
+verdadero = True
+falso = False
+
+print(num > 2025)
+
+decimal = 14.5
+print(type(decimal))
+
+numero1 = 5
+numero2 = 6
+print(numero1+numero2)
+
+#Suscribete
