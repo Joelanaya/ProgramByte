32dc99adb9bc6bb26dc0e45bc4cdb0667b094fb9
Joelanaya <86157986+Joelanaya@users.noreply.github.com>
2024-06-13 17:31:02 -0500

    Aprende sobre números en python

---
+++ Aprendiendo_Python/numeros.py
@@ -0,0 +1,73 @@
+#OPERACIONES MATEMATICAS SIMPLES
+
+#RESTA
+
+a = 10 - 4
+print(f"El resultado de la resta es:{a} ")
+
+#SUMA 
+
+b = 5 + 5
+print(f"El resultado de la suma es: {b}")
+
+#MULTIPLICACION
+
+c = 2 * 2
+print(f"El resultado de la multiplicación es: {c}")
+
+#DIVISION
+
+d = 4 / 2
+print(f"El resultado de la división es: {d}")
+
+
+
+
+
+
+#OPERACIONES MATEMATICAS AVANZADAS
+
+#MODULUS 
+
+#SOBRA
+
+m = 7 % 4
+print(f"La sobra de 7 % 4 es: {m}")
+
+#VER SI EL NUMERO ES DIVISIBLE 
+
+md = 50 % 5
+print(f"50 % 5 = {md}, Es divisible") 
+
+#PAR O IMPAR 
+
+mP = 23 % 1
+print(f"El resultado de 23 % 1 es: {mP}, Es par") #PAR, SI EL RESULTADO ES 0 SIGNIFICA QUE ES PAR
+
+mI = 23 % 2
+print(f"El resultado de 23 % 2 es: {mI}, Es impar") # IMPAR, SI EL RESULTADO ES 1 SIGNIFICA QUE ES IMPAR
+
+mP2 = 20 % 2
+print(f"El resultado de 20 % 2 es: {mP2}, Es par") # PAR
+
+
+
+#POTENCIA 
+
+p = 20 ** 2 
+print(f"La potencia de 20 ** 2 es: {p}")
+
+p2 = 10 ** 2 
+print(f"La potencia de 10 ** 2 es: {p2}")
+
+
+#BETMAS
+
+bet = 2 + 10 * 10 + 4
+print(f"El resultado de 2 '10 * 10 + 4 es: {bet}")
+
+bet2 = (2+10) * (10+4)
+print(f"El resultado de (2+10)*(10+4) es: {bet2}")
+
+
+
